0a98351489911c46b5535df92afbad67a51c9f00
lgna <ablanco837@gmail.com>
2025-07-16 16:40:38 -0300

    first commit

---
+++ wemoove-watch Watch App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  wemoove-watch Watch App
+//
+//  Created by mac on 16/07/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ wemoove-watch Watch App/wemoove_watchApp.swift
@@ -0,0 +1,17 @@
+//
+//  wemoove_watchApp.swift
+//  wemoove-watch Watch App
+//
+//  Created by mac on 16/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct wemoove_watch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
